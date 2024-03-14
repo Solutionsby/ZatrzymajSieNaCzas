111e5f8a22c1e57b63f36510b05eb28f09c7a948
Pawel Goc <pawel.goc@solutionsby.pl>
2024-03-14 22:38:35 +0100

    navigation v1

--- src/sections/navigation/displeyNawigation/NavigationDisplay.tsx
+++ src/sections/navigation/displeyNawigation/NavigationDisplay.tsx
@@ -1,6 +1,8 @@
 import './navigation.scss'
 import { useState, useContext  } from 'react';
 import { ScrollContext } from '../../../ScrollProvider';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMobileScreen, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 
 interface NavigationDisplayProps {
@@ -24,8 +26,12 @@ export const NavigationDisplay: React.FC<NavigationDisplayProps> =({
       scrollToSection(ref);
     }
   };
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
     return (
         <div className="nav">
+            <img src="/logo/logo.png" alt="logo" onClick={handleScrollToTop}/>
             <div className={`button-nav ${isToggled ? 'active' : ''}`}>
                 <div className="menu-burger" onClick={handleToggle}>
                     <div className="bar-wrapper">
@@ -36,9 +42,17 @@ export const NavigationDisplay: React.FC<NavigationDisplayProps> =({
                 </div>
                 <div className={`links-wrapper ${isToggled ? 'active' : ''}`}>
                     <ul>
-                        <li onClick={() => handleScrollToSection(abouteMe)}>O Mnie</li>
-                        <li></li>
-                        <li></li>
+                        <li className='link' onClick={() => handleScrollToSection(abouteMe)}>O Mnie</li>
+                        <li className='link'>Usługi</li>
+                        <li className='link'>FAQ</li>
+                        <li>
+                            <div className="contact-wrapper">
+                                <a href="tel:+"><FontAwesomeIcon icon={faMobileScreen} />510-608-389</a>
+                                <a href="mailto:"><FontAwesomeIcon icon={faEnvelope} />gocek89@gmail.com</a>
+                            
+
+                            </div>
+                        </li>
                     </ul>
                 </div>
 
