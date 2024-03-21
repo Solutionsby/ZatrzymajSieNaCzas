2c900a36a880d07c41c5958fc8ae27b2b0235858
Pawel Goc <pawel.goc@solutionsby.pl>
2024-03-21 21:08:33 +0100

    landscape view

--- src/sections/navigation/displeyNawigation/NavigationDisplay.tsx
+++ src/sections/navigation/displeyNawigation/NavigationDisplay.tsx
@@ -1,5 +1,5 @@
 import './navigation.scss'
-import { useState, useContext  } from 'react';
+import { useState, useContext, useEffect  } from 'react';
 import { ScrollContext } from '../../../ScrollProvider';
 import { Contact } from '../../../contact/Contact';
 
@@ -17,6 +17,14 @@ export const NavigationDisplay: React.FC<NavigationDisplayProps> =({
     abouteMe,servicesID,groupID
   })=>{
     const [isToggled, setIsToggled] = useState(false);
+    const [isMobile, setIsMobile]=useState(window.innerWidth <= 768);
+
+
+
+const handleWindowChangeSize= () =>{
+  setIsMobile(window.innerWidth <=768);
+}
+
     const scrollToSection = useContext(ScrollContext);
 
     const handleToggle = () => {
@@ -30,31 +38,56 @@ export const NavigationDisplay: React.FC<NavigationDisplayProps> =({
   const handleScrollToTop = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
+
+useEffect(()=>{
+  window.addEventListener('resize', handleWindowChangeSize);
+  return()=>{
+    window.removeEventListener('resize',handleWindowChangeSize);
+  }
+}, []);
+
+
+
     return (
         <div className="nav">
-            <img src="/logo/logo.png" alt="logo" onClick={handleScrollToTop}/>
+          <img src="/logo/logo.png" alt="logo" onClick={handleScrollToTop}/>
+          
+          {isMobile? (
             <div className={`button-nav ${isToggled ? 'active' : ''}`}>
-                <div className="menu-burger" onClick={handleToggle}>
-                    <div className="bar-wrapper">
-                        <div className={`bar one ${isToggled ? 'active' : ''}`}></div>
-                        <div className={`bar two ${isToggled ? 'active' : ''}`}></div>
-                        <div className={`bar three ${isToggled ? 'active' : ''}`}></div>
-                    </div>
+            <div className="menu-burger" onClick={handleToggle}>
+                <div className="bar-wrapper">
+                    <div className={`bar one ${isToggled ? 'active' : ''}`}></div>
+                    <div className={`bar two ${isToggled ? 'active' : ''}`}></div>
+                    <div className={`bar three ${isToggled ? 'active' : ''}`}></div>
                 </div>
-                <div className={`links-wrapper ${isToggled ? 'active' : ''}`}>
-                    <ul>
-                        <li className='link' onClick={() => handleScrollToSection(abouteMe)}>O Mnie</li>
-                        <li className='link'  onClick={() => handleScrollToSection(servicesID)} >Usługi</li>
-                        <li className='link' onClick={() => handleScrollToSection(groupID)} >Grupa wsparcia</li>
-                        <li>
-                            <div className="contact-wrapper">
-                            <Contact isAdress={false}/>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-
             </div>
+            <div className={`links-wrapper ${isToggled ? 'active' : ''}`}>
+                <ul>
+                    <li className='link' onClick={() => handleScrollToSection(abouteMe)}>O Mnie</li>
+                    <li className='link'  onClick={() => handleScrollToSection(servicesID)} >Usługi</li>
+                    <li className='link' onClick={() => handleScrollToSection(groupID)} >Grupa wsparcia</li>
+                    <li>
+                        <div className="contact-wrapper">
+                        <Contact isAdress={false}/>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
+
+
+          ):(
+            <div className="navigation-landscape">
+              <ul className='landscape-list'>
+                    <li className='link' onClick={() => handleScrollToSection(abouteMe)}>O Mnie</li>
+                    <li className='link'  onClick={() => handleScrollToSection(servicesID)} >Usługi</li>
+                    <li className='link' onClick={() => handleScrollToSection(groupID)} >Grupa wsparcia</li>
+                </ul>
+            </div>
+          )}
+
         </div>
     )
     
